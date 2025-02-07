7e7cc10377d0bb1d1e51ecd4f8c229a37b163c0e
Jay Suryavanshi <98643211+jaysuryawanshi65@users.noreply.github.com>
2025-02-07 20:47:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -52,7 +52,6 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       const response = await fetch(
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBr9Eux7OBl_OzHpZfA-dcpr2V2Zvxia0M`,
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=`,
         {
           method: "POST",
           headers: {
